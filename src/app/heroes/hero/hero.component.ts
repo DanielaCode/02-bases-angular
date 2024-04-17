50756e77e8f1c2175b0347a5c21850c55afcb21a
Daniela <danielagp96@gmail.com>
2024-04-17 13:25:17 -0600

    Explaining directive *ngIf with an example, adding and removing elements to the DOM in base of a condition

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -26,8 +26,17 @@ export class HeroComponent {
   changeAge():void{
     this.age=20;
   }
+
+  resetForm():void{
+     this.name="ironman";
+     this.age=45;
+  }
 }
 //NOTE - en angular tiene two way data binding lo que quiere decir que lo que esta del lado
 //de la clase actualiza lo que esta en el html y lo que esta en el html puede actualizar lo que esta en la clase
 //sinembargo se recomienda mantenerlo en one way data binding donde la clase es la que manda
 //LAS CLASES EXPLONEN LA INFO A EL HTML
+
+//!SECTION recordar que las manipulaciones directas al DOM no estan detectadas por el
+//ciclo de deteccion de cambios de angular, por ende no cambiaria la interfaz asi de facil
+//y empezaria a generar problemas
