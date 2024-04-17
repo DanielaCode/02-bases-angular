6c5f71fa0e0f0463522ac45bc9901cb5848fb511
Daniela <danielagp96@gmail.com>
2024-04-17 17:18:14 -0600

    Input() to define a property that can be passed by the parent component to the child component, main page pass an array of characters to dbz-list component

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
-  templateUrl: './main-page.component.html'
+  templateUrl: './main-page.component.html',
 })
 
 export class MainPageComponent {
-
+  public characters: Character[] = [
+    {
+      name:"krillin",
+      power:500
+    },
+    {
+      name:"goku",
+      power:9500
+    },
+    {
+      name:"vegeta",
+      power:7000
+    }
+  ];
 }
