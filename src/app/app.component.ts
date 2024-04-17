ec5056654d598716624ec65152302501ff3ca9ed
Daniela <danielagp96@gmail.com>
2024-04-17 12:09:36 -0600

    Moving counter logic and the html to counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,9 @@ export class AppComponent {
   //SECTION - estas propiedades estan bindeadas a el html si cambian aca cambian alla
 
   //NOTE - sintaxis corta de angular
-  title = '02-bases-angular my primer app de angular';
+  title = 'Counter';
 
   //NOTE - pero es lo mismo que
   public someText:string="segundo texto de ejemplo"
-  public counter:number=10;
 
-  //SECTION - para agregar funcionalidad a los botones, con metodos
-  increaseBy(value:number):void{
-    this.counter+=value;
-  }
-  decreaseBy(value:number):void{
-    this.counter-=value;
-  }
-  resetCounter(){
-    this.counter=10;
-  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector:"app-counter",
-  template:"<h1>Hola soy el nuevo counter component</h1>"
+  selector: 'app-counter',
+  template: `
+    <h1>Counter</h1>
+    <p>Counter: {{ counter }}</p>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="decreaseBy(1)">-1</button>
+    <button (click)="resetCounter()">Reset</button>
+  `,
 })
-export class CounterComponent{
+export class CounterComponent {
+  public counter: number = 10;
 
+  //SECTION - para agregar funcionalidad a los botones, con metodos
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  decreaseBy(value: number): void {
+    this.counter -= value;
+  }
+  resetCounter() {
+    this.counter = 10;
+  }
 }
