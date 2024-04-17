6423df687dc01a19836ec64ff987b511b6ddabbf
Daniela <danielagp96@gmail.com>
2024-04-17 12:01:17 -0600

    Creating a new counter component with an inline template

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector:"app-counter",
+  template:"<h1>Hola soy el nuevo counter component</h1>"
+})
+export class CounterComponent{
+
+}
