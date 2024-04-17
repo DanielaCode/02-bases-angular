74a828d938efcf837dea63c1e72f9c6d6e205d55
Daniela <danielagp96@gmail.com>
2024-04-17 13:03:18 -0600

    Explaining the one and two way binding, the recommended one and using expresions in HTML as invoke a method or execute a getter

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name:string="ironman";
   public age:number=45;
+
+  //NOTE - los getters permiten ser accedidos como propiedades osea sin ()
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  //NOTE - Juntar 2 propiedades
+  gerHeroDescription():string{
+    return `name: ${this.name} , age: ${this.age}`;
+  }
 }
+//NOTE - en angular tiene two way data binding lo que quiere decir que lo que esta del lado
+//de la clase actualiza lo que esta en el html y lo que esta en el html puede actualizar lo que esta en la clase
+//sinembargo se recomienda mantenerlo en one way data binding donde la clase es la que manda
+//LAS CLASES EXPLONEN LA INFO A EL HTML
