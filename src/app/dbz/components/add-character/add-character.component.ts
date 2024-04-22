094eeff574f478e042542a7198d05d99fd8ef7b3
Daniela <danielagp96@gmail.com>
2024-04-22 07:51:43 -0600

    adding a missing id

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
-
+import { v4 as uuid } from 'uuid';
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
@@ -13,6 +13,7 @@ export class AddCharacterComponent {
   public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
   public character:Character={
+    id: uuid(),
     name: '',
     power: 0
   }
